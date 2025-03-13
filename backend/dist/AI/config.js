083feaaaf7568e11a449255bb48f25d2965ad6a2
atk-titan <gauravmane8451@gmail.com>
2025-03-13 18:54:14 +0530

    backend complete

--- backend/dist/AI/config.js
+++ backend/dist/AI/config.js
@@ -4,15 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const openai_1 = __importDefault(require("openai"));
-// const openai = new OpenAI({
-//     baseURL: "https://openrouter.ai/api/v1",
-//     apiKey: process.env.API_KEY,
-//     defaultHeaders: {
-//       "HTTP-Referer": "https://github.com/OpenRouterTeam/openrouter-examples",
-//     },
-// });
 const openai = new openai_1.default({
-    baseURL: 'https://api.sree.shop/v1',
-    apiKey: process.env.CLAUDE_KEY // Replace with your API key  
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.API_KEY,
+    defaultHeaders: {
+        "HTTP-Referer": "https://github.com/OpenRouterTeam/openrouter-examples",
+    },
 });
+// const openai = new OpenAI({
+//     baseURL: 'https://beta.sree.shop/v1',
+//     apiKey: process.env.CLAUDE_KEY  // Replace with your API key  
+// });
 module.exports = openai;
